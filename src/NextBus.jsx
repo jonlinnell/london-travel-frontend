2664d4e120a69e4e9f42b079df7a948df58adf1e
Jon Linnell <jon@london-staff-2-59.lut.ac.uk>
2017-04-11 17:04:32 +0100

    Added next bus info

---
+++ src/NextBus.jsx
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import _ from 'lodash';
+import NextBusInfo from './NextBusInfo';
+import config from './config';
+
+class NextBus extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { 'busData' : [] };
+  };
+  componentWillMount() {
+    let component = this;
+    axios.get('http://countdown.api.tfl.gov.uk/interfaces/ura/instant_V1', {
+      'params' : {
+        'StopCode1' : config.busStopCode,
+        'ReturnList' : 'EstimatedTime,LineID,DestinationName,StopPointName'
+      }
+    })
+    .then(function(response) {
+
+      var formattedData = JSON.parse("[" + response.data.replace(/]/g, "],").replace(/\],$/, "]").toString() + "]");
+      component.setState({ 'busData' : formattedData });
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+  };
+  render() {
+    const buses = _.sortBy(this.state.busData, function(i) { return i[4]; }).map(function(bus) {
+      return <NextBusInfo bus={bus} />
+    });
+    return (
+      <div className="list-group">
+        <a href="#" className="list-group-item list-group-item-action flex-column align-items-start">
+          <div className="d-flex w-100 justify-content-between">
+            <h5 className="mb-1">388 Bus Route from HereEast</h5>
+          </div>
+        </a>
+        {buses}
+        <a href="#" className="list-group-item list-group-item-action flex-column align-items-start">
+          <div className="d-flex w-100 justify-content-between">
+            <small>Powered by TfL Open Data</small>
+          </div>
+        </a>
+      </div>
+    );
+  }
+}
+
+export default NextBus;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import TubeStatus from './TubeStatus';
 import SFADepartures from './SFADepartures';
 import STPDepartures from './STPDepartures';
 import NationalRailStatus from './NationalRailStatus';
+import NextBus from './NextBus';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -11,6 +12,6 @@ ReactDOM.render( <TubeStatus />, document.getElementById('TubeStatus') );
 ReactDOM.render( <SFADepartures />, document.getElementById('SFADepartures') );
 ReactDOM.render( <STPDepartures />, document.getElementById('STPDepartures') );
 ReactDOM.render( <NationalRailStatus />, document.getElementById('NationalRailStatus') );
+ReactDOM.render( <NextBus />, document.getElementById('NextBus') );
 //ReactDOM.render( <LocalStationStatus />, document.getElementById('LocalStationStatus') );
-//ReactDOM.render( <LocalBusTimes />, document.getElementById('LocalBusTimes') );
 //ReactDOM.render( <LocalBikesStatus />, document.getElementById('LocalBikesStatus') );
