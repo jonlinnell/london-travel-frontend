7ff5a2abbbb41e814d8ce3a2372e7af545dad012
Jon Linnell <jon@Epicurus.local>
2017-04-14 11:28:10 +0100

    Fix bus spinner

--- src/NextBus.jsx
+++ src/NextBus.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import _ from 'lodash';
-import Spinner from 'react-spinner';
+import Spinner from 'react-spinjs';
 import NextBusInfo from './NextBusInfo';
 import config from './config';
 
