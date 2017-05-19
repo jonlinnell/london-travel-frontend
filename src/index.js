70c470a621bef2bfa2266e64fd2e231ec14fcb6a
Jon Linnell <linnell.jon@gmail.com>
2017-05-19 20:24:02 +0100

    Removed events + router

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 /* eslint-disable class-methods-use-this */
-import React, { Component } from 'react';
+import React from 'react';
 import { render } from 'react-dom';
-import { BrowserRouter, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import TubeStatus from './TubeStatus';
@@ -12,39 +11,21 @@ import NextBus from './NextBus';
 
 import './index.css';
 
-// Pages
-class TravelSummary extends Component {
-  render() {
-    return (
-      <div className='row'>
-        <div className='col-lg-5'>
-          <TubeStatus />
-        </div>
-        <div className='col-lg-4'>
-          <SFADepartures />
-          <STPDepartures />
-        </div>
-        <div className='col-lg-3'>
-          <NationalRailStatus />
-          <NextBus />
-        </div>
-      </div>
-    );
-  }
-}
-
-class Events extends Component {
-  render() {
-    return (<h4>Working on it...</h4>);
-  }
-}
-
 render(
-  <BrowserRouter>
-    <div className='container-fluid'>
-      <Route path='/travel' component={TravelSummary} />
-      <Route path='/events' component={Events} />
+  <div className='container-fluid'>
+    <div className='row'>
+      <div className='col-lg-5'>
+        <TubeStatus />
+      </div>
+      <div className='col-lg-4'>
+        <SFADepartures />
+        <STPDepartures />
+      </div>
+      <div className='col-lg-3'>
+        <NationalRailStatus />
+        <NextBus />
+      </div>
     </div>
-  </BrowserRouter>,
+  </div>,
   document.getElementById('root')
 );
