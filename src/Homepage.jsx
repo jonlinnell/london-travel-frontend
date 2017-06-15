b4a397b7d3fcf186b909bf5a8c58007fb5ca0fc6
Jon Linnell <linnell.jon@gmail.com>
2017-06-15 23:29:03 +0100

    Origin selector

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -1,11 +1,79 @@
 /* eslint-disable class-methods-use-this */
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Redirect } from 'react-router-dom';
+import IconRight from 'material-ui/svg-icons/navigation/arrow-forward';
+import IconLeft from 'material-ui/svg-icons/navigation/arrow-back';
+import Paper from 'material-ui/Paper';
+import { ListItem } from 'material-ui/List';
+import { BottomNavigation, BottomNavigationItem } from 'material-ui/BottomNavigation';
 import TubeStatus from './TubeStatus';
 import Departures from './Departures';
 import NationalRailStatus from './NationalRailStatus';
 import NextBus from './NextBus';
 
+const LinkStyle = {
+  textAlign: 'center'
+};
+
+const BottomNavigationStyle = {
+  marginBottom: '2rem',
+  clear: 'both'
+};
+
+class OriginLondon extends Component {
+  render() {
+    return (
+      <div>
+        <Departures
+          title='Stratford Intl. Departures'
+          subtitle='Trains to London St. Pancras'
+          station='SFA'
+          destination='STP'
+        />
+        <Departures
+          title='London St. Pancras Departures'
+          subtitle='Trains calling at Loughborough'
+          station='STP'
+          destination='LBO'
+        />
+      </div>
+    );
+  }
+}
+
+class OriginLboro extends Component {
+  render() {
+    return (
+      <div>
+        <Departures
+          title='Loughborough Departures'
+          subtitle='Trains to London St. Pancras'
+          station='LBO'
+          destination='STP'
+        />
+        <Departures
+          title='London St. Pancras Departures'
+          subtitle='Trains calling at Stratford International'
+          station='STP'
+          destination='SFA'
+        />
+      </div>
+    );
+  }
+}
+
 export default class Homepage extends Component {
+  constructor() {
+    super();
+    this.state = { selectedIndex: 0 };
+  }
+
+  selectOrigin = index => this.setState({ selectedIndex: index });
+
   render() {
     return (
       <div className='container-fluid'>
@@ -14,18 +82,38 @@ export default class Homepage extends Component {
             <TubeStatus />
           </div>
           <div className='col-lg-4'>
-            <Departures
-              title='Stratford Intl. Departures'
-              subtitle='Trains to London St. Pancras'
-              station='SFA'
-              destination='STP'
-            />
-            <Departures
-              title='London St. Pancras Departures'
-              subtitle='Trains calling at Loughborough'
-              station='STP'
-              destination='LBO'
-            />
+            <Router>
+              <div>
+                <Paper>
+                  <ListItem
+                    primaryText='Journey Selector'
+                    secondaryText='Which journey are you taking today?'
+                  />
+                  <BottomNavigation
+                    style={BottomNavigationStyle}
+                    selectedIndex={this.state.selectedIndex}
+                  >
+                    <BottomNavigationItem
+                      label='London to Lboro'
+                      icon={<IconRight />}
+                      onTouchTap={() => this.selectOrigin(0)}
+                      style={LinkStyle}
+                      containerElement={<Link to="/origin/london" />}
+                    />
+                    <BottomNavigationItem
+                      label='Lboro to London'
+                      icon={<IconLeft />}
+                      onTouchTap={() => this.selectOrigin(1)}
+                      style={LinkStyle}
+                      containerElement={<Link to="/origin/lboro" />}
+                    />
+                  </BottomNavigation>
+                </Paper>
+                <Route path='/origin/lboro' component={OriginLboro} />
+                <Route path='/origin/london' component={OriginLondon} />
+                <Redirect to='/origin/london' />
+              </div>
+            </Router>
           </div>
           <div className='col-lg-3'>
             <NextBus />
