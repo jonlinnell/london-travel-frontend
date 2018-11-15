437ec6b99fbfc404449555c28157acc04c17d4f6
Jon Linnell <linnell.jon@gmail.com>
2018-11-15 14:42:40 +0000

    Add extended Tube line status information

---
+++ client/src/components/TubeLineExtendedStatus.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import qh from 'quick-hash'
+import styled from 'styled-components'
+
+const LineStatusList = styled.ul`
+  list-style: none;
+
+  padding: 12px;
+  margin: 12px -10px 0 -10px;
+
+  background-color: rgb(255, 255, 255);
+
+  color: rgb(0, 0, 0);
+
+  & > :last-child p {
+    margin-bottom: 12px;
+  }
+`
+
+const LineStatusTitle = styled.h3`
+  margin: 0;
+  margin-bottom: 6px;
+  font-size: 1.2rem;
+`
+
+const LineStatusDescription = styled.p`
+  font-size: 1rem;
+  margin-bottom: 24px;
+`
+
+export default ({ lineStatuses, toggleShowExtendedLineStatus }) => (
+  <LineStatusList onClick={toggleShowExtendedLineStatus}>
+    {
+      lineStatuses.map(status => (
+        <li key={qh(status.reason)}>
+          <LineStatusTitle>{ status.statusSeverityDescription }</LineStatusTitle>
+          <LineStatusDescription>{ status.reason }</LineStatusDescription>
+        </li>
+      ))
+    }
+  </LineStatusList>
+)
--- client/src/components/TubeLineInfo.jsx
+++ client/src/components/TubeLineInfo.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { Component } from 'react'
 import styled from 'styled-components'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCheck } from '@fortawesome/free-solid-svg-icons'
+
+import TubeLineExtendedStatus from './TubeLineExtendedStatus'
 
 const LineContainer = styled.div`
   padding: 12px;
@@ -11,11 +13,6 @@ const LineContainer = styled.div`
 
   background-color: ${({ id, theme }) => theme.lines[id].background};
   color: ${({ id, theme }) => theme.lines[id].text};
-
-  display: flex;
-  flex-wrap: nowrap;
-  justify-content: space-between;
-  align-items: center;
 `
 
 const LineName = styled.h4`
@@ -30,21 +27,57 @@ const LineStatus = styled.p`
   font-family: 'DIN Extlight';
 `
 
-const TubeLineInfo = ({ line: { id, name, lineStatuses } }) => (
-  <LineContainer id={id}>
-    <LineName>{name}</LineName>
-    <LineStatus>
-      {
-        lineStatuses[0].statusSeverityDescription === 'Good Service'
-          ? <FontAwesomeIcon icon={faCheck} />
-          : lineStatuses
-            .map(status => status.statusSeverityDescription)
-            .filter((v, i, a) => a.indexOf(v) === i) // Unique strings only
-            .map(i => i.replace(' ', '\xa0')) // Prevent status messages getting split over lines
-            .join(', ')
-      }
-    </LineStatus>
-  </LineContainer>
-)
+const Summary = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+  justify-content: space-between;
+  align-items: center;
+`
+
+class TubeLineInfo extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      showExtendedLineStatus: false,
+    }
+
+    this.toggleShowExtendedLineStatus = this.toggleShowExtendedLineStatus.bind(this)
+  }
+
+  toggleShowExtendedLineStatus = () => this.setState(state => ({
+    showExtendedLineStatus: !state.showExtendedLineStatus,
+  }))
+
+  render() {
+    const { showExtendedLineStatus } = this.state
+    const { line: { id, name, lineStatuses } } = this.props
+
+    return (
+      <LineContainer id={id}>
+        <Summary onClick={this.toggleShowExtendedLineStatus}>
+          <LineName>{name}</LineName>
+          <LineStatus>
+            {
+              lineStatuses[0].statusSeverityDescription === 'Good Service'
+                ? <FontAwesomeIcon icon={faCheck} />
+                : lineStatuses
+                  .map(status => status.statusSeverityDescription)
+                  .filter((v, i, a) => a.indexOf(v) === i) // Unique strings only
+                  .map(i => i.replace(' ', '\xa0')) // Prevent status messages getting split over lines
+                  .join(', ')
+            }
+          </LineStatus>
+        </Summary>
+        {
+          showExtendedLineStatus
+            ? <TubeLineExtendedStatus lineStatuses={lineStatuses} toggleShowExtendedLineStatus={this.toggleShowExtendedLineStatus}/>
+            : null
+        }
+      </LineContainer>
+    )
+  }
+}
 
 export default TubeLineInfo
