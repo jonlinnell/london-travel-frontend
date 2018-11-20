0a8d226f3316c1730e598d1c0e27112356970643
Jon Linnell <linnell.jon@gmail.com>
2018-11-20 10:04:03 +0000

    Update RGB lib funcs

--- client/src/lib/colours.js
+++ client/src/lib/colours.js
@@ -1,27 +1,21 @@
-export const lighten = (rgbString, amount) => {
-  const numberMatch = /([0-9])+, ?([0-9])+, ?([0-9])+ ?/g
+const numberMatch = /([0-9])+, ?([0-9])+, ?([0-9])+ ?/g
 
-  return `rgb(${numberMatch
-    .exec(rgbString)[0]
-    .split(/, ?/)
-    .map(colour => Math
-      .round((
-        parseInt(colour, 10) + amount) > 255
-        ? 255
-        : parseInt(colour, 10) + amount))
-  })`
-}
+export const lighten = (rgbString, amount) => `rgb(${numberMatch
+  .exec(rgbString)[0]
+  .split(/, ?/)
+  .map(colour => Math
+    .round((
+      parseInt(colour, 10) + amount) > 255
+      ? 255
+      : parseInt(colour, 10) + amount))
+})`
 
-export const darken = (rgbString, amount) => {
-  const numberMatch = /([0-9])+, ?([0-9])+, ?([0-9])+ ?/g
-
-  return `rgb(${numberMatch
-    .exec(rgbString)[0]
-    .split(/, ?/)
-    .map(colour => Math
-      .round((
-        parseInt(colour, 10) - amount) < 0
-        ? 0
-        : parseInt(colour, 10) - amount))
-  })`
-}
+export const darken = (rgbString, amount) => `rgb(${numberMatch
+  .exec(rgbString)[0]
+  .split(/, ?/)
+  .map(colour => Math
+    .round((
+      parseInt(colour, 10) - amount) < 0
+      ? 0
+      : parseInt(colour, 10) - amount))
+})`
