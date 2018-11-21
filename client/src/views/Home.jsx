f3769890360fb2af6fbb0fbe5b8ee8f32c3a8f7a
Jon Linnell <linnell.jon@gmail.com>
2018-11-21 08:25:30 +0000

    Anim tweaks

--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -49,7 +49,7 @@ const StyledButtonList = styled.ul`
 
 const Buttons = posed(StyledButtonList)({
   enter: {
-    opacity: 1, delayChildren: 400, staggerChildren: 60,
+    opacity: 1, staggerChildren: 60,
   },
   exit: { opacity: 0 },
 })
