513b01aadbd54823c7107eeef88680fd8c6d2914
Jon Linnell <linnell.jon@gmail.com>
2018-11-22 13:25:49 +0000

    Escape search strings for station lookup

--- server/lib/searchStations.js
+++ server/lib/searchStations.js
@@ -1,5 +1,9 @@
 const stations = require('./railStations.json')
 
 module.exports = searchString => new Promise(resolve => resolve(
-  stations.filter(station => station.name.toLowerCase().match(searchString.toLowerCase()))
+  stations.filter(station => station.name
+    .toLowerCase()
+    .match(searchString
+      .toLowerCase()
+      .replace(/[.*+?^${}()|[\]\\]/g, '\\$&')))
 ))
