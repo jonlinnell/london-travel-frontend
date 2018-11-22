fc85380d7dd344bb2744f31888a1d86c49fa04ec
Jon Linnell <linnell.jon@gmail.com>
2018-11-22 12:46:12 +0000

    Add per-request cacheing

--- server/routes.js
+++ server/routes.js
@@ -1,13 +1,29 @@
+const cache = require('memory-cache')
+
 const fetchTubeStatus = require('./lib/fetchTubeStatus')
 const getBusDepartures = require('./lib/getBusDepartures')
 const getRailDepartureBoard = require('./lib/getRailDepartureBoard')
 const searchStations = require('./lib/searchStations')
 
+const CACHE_PREFIX = '__server__'
+const CACHE_TUBE = `${CACHE_PREFIX}-tube`
+const CACHE_BUS = `${CACHE_PREFIX}-bus`
+const CACHE_TRAIN = `${CACHE_PREFIX}-train`
+
 const routes = (app) => {
   app.get('/tube', (req, res) => {
-    fetchTubeStatus()
-      .then(data => res.json(data))
-      .catch(error => res.status(500).send(`An error occurred loading the tube data: ${error}`))
+    const cachedData = cache.get(CACHE_TUBE)
+
+    if (cachedData) {
+      res.json(cachedData)
+    } else {
+      fetchTubeStatus()
+        .then((data) => {
+          cache.put(CACHE_TUBE, data, 60000)
+          res.json(data)
+        })
+        .catch(error => res.status(500).send(`An error occurred loading the tube data: ${error}`))
+    }
   })
 
   app.get('/bus/:stopCode/:limit?', (req, res) => {
@@ -15,19 +31,39 @@ const routes = (app) => {
 
     if (!req.params.stopCode) res.status(400).send('No stop code provided.')
 
-    getBusDepartures(req.params)
-      .then(data => res.json(data))
-      .catch(error => res.status(500).send(`An error occurred loading the bus data: ${error}`))
+    const cacheRequestKey = `${CACHE_BUS}-${req.params.stopCode}`
+    const cachedData = cache.get(cacheRequestKey)
+
+    if (cachedData) {
+      res.json(cachedData)
+    } else {
+      getBusDepartures(req.params)
+        .then((data) => {
+          cache.put(cacheRequestKey, data, 20000)
+          res.json(data)
+        })
+        .catch(error => res.status(500).send(`An error occurred loading the bus data: ${error}`))
+    }
   })
 
   app.get('/rail/:station/:destination?', (req, res) => {
-    // const { station, destination } = req.params
+    const { station, destination } = req.params
 
-    if (!req.params.station) res.status(400).send('No station specified.')
+    if (!station) res.status(400).send('No station specified.')
 
-    getRailDepartureBoard(req.params)
-      .then(data => res.json(data))
-      .catch(error => res.status(500).send(error))
+    const cacheRequestKey = `${CACHE_TRAIN}-${station}${destination ? `-${destination}` : ''}`
+    const cachedData = cache.get(cacheRequestKey)
+
+    if (cachedData) {
+      res.json(cachedData)
+    } else {
+      getRailDepartureBoard(req.params)
+        .then((data) => {
+          cache.put(cacheRequestKey, data, 30000)
+          res.json(data)
+        })
+        .catch(error => res.status(500).send(error))
+    }
   })
 
   app.get('/searchStations/:string', (req, res) => {
