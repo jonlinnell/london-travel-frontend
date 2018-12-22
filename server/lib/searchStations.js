88832c2d9f2eedc046af01c0d5305ef5cc5a5c95
Jon Linnell <linnell.jon@gmail.com>
2018-12-22 17:20:52 +0000

    MAJOR split from london-travel

--- server/lib/searchStations.js
+++
@@ -1,9 +0,0 @@
-const stations = require('./railStations.json')
-
-module.exports = searchString => new Promise(resolve => resolve(
-  stations.filter(station => station.name
-    .toLowerCase()
-    .match(searchString
-      .toLowerCase()
-      .replace(/[.*+?^${}()|[\]\\]/g, '\\$&')))
-))
